ac56f762964492450674a659a36674a7384904d8
inthewaves <inthewaves@pm.me>
2026-02-19 18:35:41 -0800

    use Jetpack's Navigation component

--- src/app/grapheneos/gmscompat/MainActivity.kt
+++ src/app/grapheneos/gmscompat/MainActivity.kt
@@ -3,23 +3,33 @@ package app.grapheneos.gmscompat
 import android.app.compat.gms.GmsCompat
 import android.content.Intent
 import android.ext.PackageId
-import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
+import androidx.core.net.toUri
+import androidx.navigation.findNavController
+import androidx.navigation.createGraph
+import androidx.navigation.fragment.fragment
 
 const val USAGE_GUIDE_URL = "https://grapheneos.org/usage#sandboxed-google-play"
 
 class MainActivity : CollapsingToolbarBaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
         if (!GmsCompat.isEnabledFor(PackageId.GMS_CORE_NAME, userId)) {
-            val uri = Uri.parse(USAGE_GUIDE_URL)
+            val uri = USAGE_GUIDE_URL.toUri()
             startActivity(Intent(Intent.ACTION_VIEW, uri))
             finishAndRemoveTask()
             return
+        }
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.graph = navController.createGraph(startDestination = NavRoutes.Main) {
+            fragment<MainFragment>(NavRoutes.Main) {
+                label = getString(R.string.activity_name)
+            }
         }
     }
 }
---
+++ src/app/grapheneos/gmscompat/NavRoutes.kt
@@ -0,0 +1,10 @@
+package app.grapheneos.gmscompat
+
+/**
+ * Use this with a NavController to navigate. If arguments are desired, it would be better to
+ * use https://developer.android.com/guide/navigation/design/kotlin-dsl which requires
+ * "kotlinx_serialization_core" and "kotlinx_serialization_json" imports.
+ */
+object NavRoutes {
+    const val Main = "main"
+}

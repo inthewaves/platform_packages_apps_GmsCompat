967a53fae7e577feaa10fa304ea1a50fcc2640e0
inthewaves <inthewaves@pm.me>
2026-02-19 18:35:41 -0800

    [wip] experiment

--- src/app/grapheneos/gmscompat/MainActivity.kt
+++ src/app/grapheneos/gmscompat/MainActivity.kt
@@ -7,19 +7,38 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
+import androidx.core.net.toUri
+import androidx.navigation.findNavController
+import kotlinx.serialization.Serializable
+import androidx.navigation.createGraph
+import androidx.navigation.fragment.fragment
 
 const val USAGE_GUIDE_URL = "https://grapheneos.org/usage#sandboxed-google-play"
 
 class MainActivity : CollapsingToolbarBaseActivity() {
+
+    @Serializable
+    object MainScreen
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
         if (!GmsCompat.isEnabledFor(PackageId.GMS_CORE_NAME, userId)) {
-            val uri = Uri.parse(USAGE_GUIDE_URL)
+            val uri = USAGE_GUIDE_URL.toUri()
             startActivity(Intent(Intent.ACTION_VIEW, uri))
+            val c = androidx.navigation.fragment.NavHostFragment::javaClass.name
             finishAndRemoveTask()
             return
+        }
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.graph = navController.createGraph(
+            startDestination = MainScreen
+        ) {
+            fragment<MainFragment, MainScreen> {
+                label = getString(R.string.activity_name)
+            }
         }
     }
 }
